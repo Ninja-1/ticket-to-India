62621189546f3c33416569454c6e1dd5a5027efb
Ninja-1 <ninjavsv@gmail.com>
2023-04-28 15:01:31 +0300

    ticket-to-India
    Облегчил картинки
    Добавил пословицы

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 let randomWordsA = [
   "Сегодня ",
   "На данный момент ",
-  "Только сегодня ",
-  "Как нам стало известно, ",
+  "В этом сезоне ",
+  "Как нам только что стало известно, ",
   "Прямо сейчас ",
   "Представляете,  ",  
 ];
@@ -13,11 +13,11 @@ let randomWordsB = [
   "в Индию ",
   "в жаркие страны ",
   "в командировку от завода ",
-  "в такую даль  ",    
+  "в такую даль ",    
 ];
 let random = [
   "билетов не подвезли.",
-  "очень большая очередь.",
+  "желающих хоть отбавляй.",
   "конструкторов не пускают.",
   "поедете сами, Галина Николаевна.",
   "только на оленях.",
@@ -26,24 +26,7 @@ let random = [
   "было бы неплохо съездить.",  
 ];
 
-
-
-/*function myBtn() {
-  let num = prompt ("Ведите желаемую дату выеда (любое число)", 0);
-  if (num == null) {
-    alert("Ваш выбор очень важен для нас!");
-  } else { 
-    let randomIndexA = Math.floor(Math.random() * 6);
-    let randomIndexB = Math.floor(Math.random() * 5);
-    let randomIndexC = Math.floor(Math.random() * 8);
-    let res = randomWordsA[randomIndexA] + randomWordsB[randomIndexB] + random[randomIndexC];
-    alert(res);
-    if (confirm ("Еще сыграем?")) {
-      alert("Жми тогда на кнопку")      
-    } else { alert("Ля ты и Бука!");}
-    };  
-}*/
-
+//Рабочая ф-ция Алерта
 function myBtn() {
  
     let randomIndexA = Math.floor(Math.random() * 6);
@@ -51,11 +34,32 @@ function myBtn() {
     let randomIndexC = Math.floor(Math.random() * 8);
     let res = randomWordsA[randomIndexA] + randomWordsB[randomIndexB] + random[randomIndexC];
     alert(res);
-    if (confirm ("Еще сыграем?")) {
-      /*alert("Жми тогда на кнопку")*/  
+    if (confirm ("Еще сыграем?")) {      
       return;
     } else { alert("Ля ты и Бука!");}
-      
+};
+
+let randomWordsD = [
+  "Вот тебе, бабушка, и Юрьев день",
+  "А не пора ли нам подкрепиться?",
+  "Лиш бы ото на все кнопки понажимать!",
+  "Любопытство не порок а источник знаний",
+  "Здесь вам не тут",  
+  "Лучше водки хуже нет.",
+  "Мы выполнили все пункты: от А до Б.",
+  "Хотели как лучше, а получилось как всегда.",  
+  "Мы надеемся, что у нас не будет запоров на границе.",
+  "Я бы не стал увязывать эти вопросы так перпендикулярно.",
+];
+
+function circl() {
+  let randomIndexD = Math.floor(Math.random() * 10);
+  let res1 = randomWordsD[randomIndexD];
+  alert(res1);  
 }
+
+
+
+
 
 
